96772189551ce9235f60f9f358e18d8540c7b87a
AlexandraPauna <alexandrapauna927@yahoo.com>
2019-03-26 23:26:40 +0200

    definitions for Entities Message and Conversation added

--- Licenta/Models/Communication/Conversation.cs
+++ Licenta/Models/Communication/Conversation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,18 @@ namespace Licenta.Models.Communication
 {
     public class Conversation
     {
+        [Key]
+        public int ConversationId { get; set; }
+
+        [Required]
+        [Display(Name = "Anuntul")]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        //[Required]
+        //public string FromUser { get; set; }
+
+        public virtual ICollection<Message> Messages { get; set; }
+
     }
 }
--- Licenta/Models/Communication/Message.cs
+++ Licenta/Models/Communication/Message.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,24 @@ namespace Licenta.Models.Communication
 {
     public class Message
     {
+        [Key]
+        public int MessageId { get; set; }
+
+        [Required(ErrorMessage = "Scrieti mesajul!")]
+        //[Display(Name = "Continut")]
+        public string Content { get; set; }
+
+        bool Read { get; set; }
+
+        [Required]
+        public int ConversationId { get; set; }
+        public virtual Conversation Conversation { get; set; }
+        //public IEnumerable<Conversation> Conversations { get; set; }
+
+        [Required]
+        public string FromUserId { get; set; }
+
+        [Required]
+        public string ToUserId { get; set; }
     }
 }
