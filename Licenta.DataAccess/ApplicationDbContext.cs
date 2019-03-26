87cd7c95e5b03617c0e4ca8eb327f8bc1eb3241b
GavriliucRoman <48831639+GavriliucRoman@users.noreply.github.com>
2019-03-26 20:59:44 +0200

    Fix build

---
+++ Licenta.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using Licenta.Common.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Licenta.DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", false)
+        {
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<ProductState> ProductState { get; set; }
+        public DbSet<DeliveryCompany> DeliveryCompanies { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
