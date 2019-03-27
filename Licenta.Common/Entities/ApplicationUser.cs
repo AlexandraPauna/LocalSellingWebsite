e4d3502e90808a1cb860c334997d9077c0bc00b9
GavriliucRoman <48831639+GavriliucRoman@users.noreply.github.com>
2019-03-27 13:35:16 +0200

    Adaugare fisier lipsa

---
+++ Licenta.Common/Entities/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Licenta.Common.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CityId { get; set; }
+        public virtual City City { get; set; }
+        public IEnumerable<SelectListItem> Cities { get; set; }
+        public byte[] UserPhoto { get; set; }
+
+        public IEnumerable<SelectListItem> AllRoles { get; set; }
+    }
+}
