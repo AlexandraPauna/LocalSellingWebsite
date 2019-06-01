1f68f9d58da43e128de459b8fde0b34879926219
AlexandraPauna <alexandrapauna927@yahoo.com>
2019-06-01 21:46:50 +0300

    Interests/Index design

--- Licenta/Controllers/InterestsController.cs
+++ Licenta/Controllers/InterestsController.cs
@@ -28,11 +28,63 @@ namespace Licenta.Controllers
                                  select ints).OrderByDescending(x => x.Date);
 
                 var model = new InterestViewModel { Interests = interests.ToList() };
-                
+
+                var unreadMessages = (from mess in _db.Messages
+                                      where mess.ReceiverId == currentUser && mess.Read == false
+                                      select mess).Count();
+                ViewBag.UnreadMessages = unreadMessages;
+
+                var adViews = _db.Products.GroupBy(a => a.UserId).Select(x => new
+                {
+                    UserId = x.Key,
+                    Value = x.Sum((c => c.Views)),
+
+                }).Where(x => x.UserId == currentUser).FirstOrDefault();
+                if (adViews != null)
+                    ViewBag.AdViews = (int)adViews.Value;
+                else
+                    ViewBag.AdViews = 0;
+
+                var nrAds = _db.Products.Where(x => x.UserId == currentUser).Count();
+                ViewBag.NrAds = nrAds;
+
+                var nrRatings = _db.Ratings.Where(x => x.RatedUserId == currentUser).Count();
+                ViewBag.NrRatings = nrRatings;
+
+                var nrInterests = _db.Interests.Where(x => x.UserId == currentUser).Count();
+                ViewBag.NrInterests = nrInterests;
+
                 return View(model);
             }
 
-               
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string[] productIDs)
+        {
+
+            foreach (string productID in productIDs)
+            {
+                int prodID;
+                if (int.TryParse(productID, out prodID))
+                {
+                    //int prodId = int.Parse(productID);
+                    var currentUserId = User.Identity.GetUserId();
+                    var interest = from interests in _db.Interests
+                                   where interests.ProductId == prodID && interests.UserId == currentUserId
+                                   select interests;
+                    if (interest.Count() == 1)
+                    {
+                        _db.Interests.Remove(interest.First());
+                    }
+                }
+                
+
+            }
+            _db.SaveChanges();
+
+            return Json(Url.Action("Index", "Interests"));
         }
     }
 }
